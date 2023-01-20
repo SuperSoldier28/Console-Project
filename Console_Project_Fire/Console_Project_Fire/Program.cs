083118a6d6939ab332de06d3fc79458f6532dc10
kimdoik <anemone955@naver.com>
2023-01-20 21:48:52 +0900

    시작을 해봅시다!

---
+++ Console_Project_Fire/Console_Project_Fire/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write($"당신의 이름은 무엇인가요? : {Console.ReadLine()}");
+
+        // 게임 루프
+        while (true)
+        {
+            //==============Render================
+
+            //=============ProcessInput===========
+
+            //=============Update=================
+        }
+    }
+}
