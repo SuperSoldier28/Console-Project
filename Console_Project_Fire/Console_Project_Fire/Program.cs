2da9911acbc7823b00b9394b837b2db48b5646c9
kimdoik <anemone955@naver.com>
2023-01-23 16:02:18 +0900

    시작 내용 초기 단계

--- Console_Project_Fire/Console_Project_Fire/Program.cs
+++ Console_Project_Fire/Console_Project_Fire/Program.cs
@@ -2,8 +2,15 @@
 {
     static void Main()
     {
-        Console.Write($"당신의 이름은 무엇인가요? : {Console.ReadLine()}");
 
+        Console.WriteLine("안녕하세요!");
+        Console.WriteLine("망상의 나라 [찐따인 내가  도내 최상위 쿨뷰티 초S급 미녀 일진과 연애를...?]에 오신 것을 환영합니다.");
+        Console.WriteLine("게임 진행을 위해 이름을 작성하고 Enter키를 눌러주세요");
+        Console.Write("이름 : ");
+
+        string name = Console.ReadLine();
+        Console.Write($"사용자의 이름은 : '{name}'으로 설정 되었습니다.");
+        
         // 게임 루프
         while (true)
         {
